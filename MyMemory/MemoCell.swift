a390f62b2e3efa9d247da3e37d18e10346d3e0b9
김철 <charlie@galaxia.co.kr>
2018-12-13 18:19:14 +0900

    프로젝트 생성

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by user on 2018. 12. 13..
+//  Copyright © 2018년 galaxia communications. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
